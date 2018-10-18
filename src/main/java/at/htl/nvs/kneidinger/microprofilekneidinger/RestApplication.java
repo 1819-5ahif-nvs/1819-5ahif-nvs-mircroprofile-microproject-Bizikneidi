0f1c45cebbc8ad0f1690950d99e173e9e6a5dffb
Bizi Kneidi <tobias.kneidinger@gmail.com>
2018-10-18 20:09:43 +0200

    Changed language to kotlin for RestConfig

--- src/main/java/at/htl/nvs/kneidinger/microprofilekneidinger/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package at.htl.nvs.kneidinger.microprofilekneidinger;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application {
-
-}
