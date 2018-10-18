8cf14274a1387202152785f7019c4c8677702744
Bizi Kneidi <tobias.kneidinger@gmail.com>
2018-10-18 18:01:54 +0200

    Changed project-structure

--- microprofile/src/main/java/persistence/Repository.kt
+++
@@ -1,9 +0,0 @@
-package persistence
-
-interface Repository<T> {
-    fun create(item: T): T
-    fun update(item: T)
-    fun getAll(): List<T>
-    fun getById(id: Long): T
-    fun delete(id: Long)
-}
